edf31a4c6fae96734e53a2ad4235f7c45516620b
tomv <tomv@elysium.uk>
2024-03-12 09:48:48 +0000

    Add speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
-import '../styles/styles.css';
-import { HeaderProvider } from "../contexts/headerContext"
-import { SessionProvider } from "next-auth/react"
+import "../styles/styles.css";
+import { HeaderProvider } from "../contexts/headerContext";
+import { SessionProvider } from "next-auth/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <HeaderProvider>
-          <Component {...pageProps} />
+        <SpeedInsights />
+        <Component {...pageProps} />
       </HeaderProvider>
     </SessionProvider>
-  )
+  );
 }
 
-export default MyApp; 
+export default MyApp;
