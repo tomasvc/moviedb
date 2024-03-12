53da29139604c3ce58394b94eff7345f569603ea
tomv <tomv@elysium.uk>
2024-03-12 09:53:38 +0000

    Update components to default exports

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Header } from "../../components/Header";
 import { useHeaderContext } from "../../contexts/headerContext";
 
-export const Login = () => {
+export default function Login() {
   const { open, setOpen } = useHeaderContext();
   return (
     <div>
       <Header open={open} setOpen={setOpen} />
     </div>
   );
-};
+}
