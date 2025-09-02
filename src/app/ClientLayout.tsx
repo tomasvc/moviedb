1c9d335d5803025d4b7abe6f2350853254b93372
Tomv <tomas.v0147@gmail.com>
2025-09-02 16:26:10 +0300

    UPDATE: resolve old MUI package

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { IsClientCtxProvider } from "../contexts/isClientCtx";
 import NextNProgress from "nextjs-progressbar";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 export function ClientLayout({
   children,
